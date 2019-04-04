3765bed03be7bcdc82501977b9d76617aa7a791f
Oleg Kmechak <ofcrashbash@gmail.com>
2019-04-04 20:40:37 +0200

    [MAJOR, RESEARCH]: access database research

---
+++ databaseio.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import pyodbc
+
+#Access database engine should be installed!! TODO add this nnote to documentation
+x = [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
+
+print(x)
+
+conn_str = (
+    r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+    r'DBQ=C:\Users\ofcra\Google Drive\Study\PyPRINSAS\PRINSAS Data\SANSRAW.mdb;'
+    )
+cnxn = pyodbc.connect(conn_str)
+crsr = cnxn.cursor()
+for table_info in crsr.tables(tableType='TABLE'):
+    print(table_info.table_name)
